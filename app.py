924dafb6b8402614d414880f66c1531afd2ecae9
Samuel Raj G <samuelgariki11@gmail.com>
2026-01-05 09:06:28 +0530

    update app.py and add helper and preprocessor files

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import streamlit as st
 import pandas as pd
+import preprocessor,helpper
 
-st.sidebar.radio(
+df = pd.read_csv("data/athlete_events.csv")
+region_df = pd.read_csv("data/noc_regions.csv")
+
+df = preprocessor.preprocess(df,region_df)
+
+st.sidebar.title("Olympic")
+user_menu = st.sidebar.radio(
     'Select an option',
-    ('Medal Taly', 'Overal Analysis', 'Country-wise Analysis', 'Athlete wise Analysi')
+    ('Medal Tally', 'Overal Analysis', 'Country-wise Analysis', 'Athlete wise Analysi')
 )
+
+
+if user_menu == 'Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years,country = helpper.country_year_list(df)
+
+    selected_year = st.sidebar.selectbox("Select Year", years)
+    selected_country = st.sidebar.selectbox("Select Country", country)
+
+    medal_tally = helpper.fetch_medal_tally(df,selected_year,selected_country)
+    if selected_year == "Overall" and selected_country == "Overall":
+        st.title("Overall Tally")
+    if selected_year != "Overall" and selected_country == "Overall":
+        st.title("Medal Tally in" + str(selected_year) + "Olympics")
+    if selected_year == "Overall" and selected_country != 'Overall':
+        st.title(selected_country + "overall performance")
+    if selected_country != "Overall" and  selected_year != "Overall":
+        st.title(selected_country + "performance in" + str(selected_year) + "Olympics")
+
+    st.dataframe(medal_tally)
